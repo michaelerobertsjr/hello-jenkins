5b2ff7bb7c53caca39228d2e6a6ed7de31fedf86
Michael Roberts Jr <michael.e.roberts.jr@gmail.com>
2014-12-13 21:22:29 -0800

    Switch to hello jenkins

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.send('hello jenkins');
 });
 
 app.listen(port, function(){
